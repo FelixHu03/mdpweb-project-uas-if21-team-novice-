5ad21e55333673f92b02992d34c1ffd3c70f4951
Felix <147794850+FelixHu03@users.noreply.github.com>
2024-06-25 11:28:35 +0700

    Update and rename layananTambahan.php to LayananTambahan.php

--- app/Models/LayananTambahan.php
+++ app/Models/LayananTambahan.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class layananTambahan extends Model
+class LayananTambahan extends Model
 {
     use HasFactory;
 
