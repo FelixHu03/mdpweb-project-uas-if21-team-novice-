9da0828ff91d48dcaefade6f9854d2113486bd41
Felix <147794850+FelixHu03@users.noreply.github.com>
2024-06-25 11:41:11 +0700

    Update and rename tamu.php to Tamu.php

--- app/Models/Tamu.php
+++ app/Models/Tamu.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tamu extends Model
+class Tamu extends Model
 {
     use HasFactory;
     protected $fillable = ['id_tamu','nama', 'alamat','email', 'no_telepon'];
